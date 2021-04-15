f14b8799728a5136319a742eede38ca5ca00e662
Karim Benlghalia <k.benlghalia@gmail.com>
2021-04-15 03:30:11 -0700

    set up the client app

--- server/index.js
+++ server/index.js
@@ -4,9 +4,10 @@ require('dotenv').config({path: './config/confi6g.env'})
 const config = require('./config/config.js');
 
 
-    const PORT = process.env.PORT || config.app.port || 3000;
+    const PORT = process.env.PORT || config.app.port || 5000;
     const NODE_ENV = process.env.PORT || config.app.node_env;
 
 const server = app.listen(PORT, () => {
     console.log(`Server started in ${NODE_ENV} envirement on port: ${PORT}`);
 });
+
