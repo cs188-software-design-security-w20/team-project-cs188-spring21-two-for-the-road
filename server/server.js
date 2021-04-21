5c362e5a4538e92576d4d0e2d7e82f72b83cf230
arabellekezia <arabellekezia@gmail.com>
2021-04-21 16:08:48 -0700

    Fix signup.js and server.js

--- server/server.js
+++ server/server.js
@@ -4,20 +4,20 @@
 const config = require('./config/config.js')
 
 const mongoose = require('mongoose')
-const { database: { username, password, db } } = config;
+const { app: {port, node_env}, database: { username, password, db } } = config;
 const uri = `mongodb+srv://${username}:${password}@cs188.pjfhc.mongodb.net/${db}?retryWrites=true&w=majority`;
 
 const express = require('express')
 const app = express()
-require('dotenv').config({path: './config/confi6g.env'})
+require('dotenv').config({path: './config/config.env'})
 
 const PORT = process.env.PORT || config.app.port || 5000;
 const NODE_ENV = process.env.PORT || config.app.node_env;
 
-mongoose.connect(uri, { useNewUrlParser: true }).
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}).
   catch(error => handleError(error));
   console.log("Succesfully connected to DB...")
 
-const server = app.listen(PORT, () => {
-    console.log("Server started in ${NODE_ENV} environment on port: ${PORT}");
+const server = app.listen(port, () => {
+    console.log("Server started in " + node_env + " environment on port: " + port);
 });
--- server/signup.js
+++ server/signup.js
@@ -3,14 +3,22 @@
 */
 
 const config = require('./config/config.js')
-
-const mongoose = require('mongoose')
-const { database: { username, password, db } } = config;
+// const mongoose = require('mongoose')
+const { app: {port, node_env}, database: { username, password, db } } = config;
 const uri = `mongodb+srv://${username}:${password}@cs188.pjfhc.mongodb.net/${db}?retryWrites=true&w=majority`;
-const client = require('mongodb').MongoClient.connect();
+
+const client = require('mongodb').MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const express = require('express')
+const app = express()
+require('dotenv').config({path: './config/config.env'})
 
 /*
   Function that validates that an email address only contains unicode
+
+  PARAMETERS
+   email : string
+    String of the email we want to check for validy
 */
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -21,26 +29,26 @@ function validateEmail(email) {
   Function to be called when a Student signs up - logs value into the DB
 
   PARAMETERS
-    firstName : String
+    firstName : string
       First name of student
-    lastName : String
+    lastName : string
       Last name of student
-    email : String
+    email : string
       Student's email - check that it ends with ucla.edu
-    sid : String
+    sid : string
       Student UCLA ID - 9 digits, number only
     year : Int
       Current year at UCLA
         1 - freshman, 2 - sophomore, 3 - junior, 4 - senior, 5 - supersenior & above
-    gradTerm : String
-      String consisting of 1-2 capital letter graduation quarter and
+    gradTerm : string
+      string consisting of 1-2 capital letter graduation quarter and
       2 digit graduation year (i.e. 21 is 2021, 22 is 2022, etc.)
         F   - Fall, W   - Winter, Sp  - Spring, S   - Summer
-    major : String
+    major : string
       Student's major
-    minor : String
+    minor : string
       Student's minor, NA if not applicable
-    club : String
+    club : string
       Student's associated club
     honorStudent : bool
       True if student is in an honor's student, False otherwise
@@ -56,26 +64,36 @@ function validateEmail(email) {
 async function signUpStudent(firstName, lastName, email, sid, year, gradTerm, major, minor, club, honorStudent, resume, profileImage) {
   try {
     // connect to database
-    await client.connect();
+    await client.connect()
+      .catch(error => handleError(error));
+    console.log("Succesfully connected to DB...")
+
     const database = client.db("test");
     const col = database.collection("Student");
 
     // check validity of values
-    if (typeof(firstname) != 'string' || typeof(lastName) != 'string' || typeof(major) != 'string' ||
+    if (typeof(firstName) != 'string' || typeof(lastName) != 'string' || typeof(major) != 'string' ||
     typeof(minor) != 'string' || typeof(club) != 'string' || typeof(email) != 'string' || typeof(gradTerm) != 'string' ||
     typeof(year) != 'number' || typeof(honorStudent) != 'boolean'){
       throw new TypeError("One or more parameters have the wrong type");
     }
-    if(sid.match(/^[0-9]+$/) != null || sid.length != 9){
-      throw new Error("sid can only contain digits");
+    if (year < 1 && year > 5){
+      throw new Error("year value can only be between 1 to 5");
     }
-    if (!vaildateEmail(email) && !email.includes("ucla.edu")){
+    if (!(/^\d+$/.test(sid)) || sid.length != 9){
+      throw new Error("sid should contain 9 digits");
+    }
+    if (!validateEmail(email) || !email.includes("ucla.edu")){
       throw new Error("Email contains invalid characters or is not UCLA email");
     }
-    if ((gradTerm.includes('Sp') || gradTerm.includes('F') || gradTerm.includes('W') || gradTerm.includes('S')) && !isNaN(gradTerm.slice(-2))){
+    var gradQ1 = gradTerm.charAt(0);
+    var gradQ2 = gradTerm.substring(0,1);
+    if (!(gradQ2.includes("Sp") || gradQ1.includes("F") || gradQ1.includes("W") ||
+    gradQ1.includes("S")) || !(/^\d+$/.test(gradTerm.slice(-2)))){
       throw new Error("gradTerm has invalid format")
     }
 
+    // create doc to be inserted after checking for params
     var doc = {
       firstName:firstName,
       lastName:lastName,
@@ -90,7 +108,8 @@ async function signUpStudent(firstName, lastName, email, sid, year, gradTerm, ma
       profileImage:profileImage
     };
 
-    const result = await col.insertOne(doc);
+    const result = await col.insertOne(doc)
+      .catch(error => handleError(error));
     console.log(
       `${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`,
     );
@@ -99,6 +118,8 @@ async function signUpStudent(firstName, lastName, email, sid, year, gradTerm, ma
   }
 }
 
-
-// test function
-signUpStudent("Arabelle", "Siahaan", "arabellek@ucla.edu", "604928106", "4", "Sp21", "CS", "NA", "ACM", true, "NA", "NA");
+// test function - uncomment this to test the function
+// signUpStudent("Arabelle", "Siahaan", "test@ucla.edu", "123456789", 4, "W21", "CS", "NA", "test", true, "NA", "NA");
+// signUpStudent("Arabelle", "Siahaan", "test@gmail.edu", "123456789", 4, "W21", "CS", "NA", "test", true, "NA", "NA");
+// signUpStudent("Arabelle", "Siahaan", "test@ucla.edu", "12356789", 4, "W21", "CS", "NA", "test", true, "NA", "NA");
+// signUpStudent("Arabelle", "Siahaan", "test@ucla.edu", "12356789", 4, "L21", "CS", "NA", "test", true, "NA", "NA");
